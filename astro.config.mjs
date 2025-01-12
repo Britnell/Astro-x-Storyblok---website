cb93c6a4967cf27165c642946a7a20c8b195151c
Thomas Britnell <thomas.britnell@h-d-gmbh.de>
2025-01-12 10:36:43 +0100

    astro project + tw + storyblok

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { defineConfig, envField } from "astro/config";
+import storyblok from "@storyblok/astro";
+// https://astro.build/config
+import { loadEnv } from "vite";
+
+import tailwind from "@astrojs/tailwind";
+
+const { STORYBLOK_TOKEN } = loadEnv(
+  process.env.NODE_ENV ?? "",
+  process.cwd(),
+  ""
+);
+
+export default defineConfig({
+  integrations: [storyblok({
+    accessToken: STORYBLOK_TOKEN,
+    components: {
+      page: "storyblok/Page",
+      hero: "storyblok/Hero",
+    },
+    enableFallbackComponent: true,
+    apiOptions: {
+      region: "eu",
+    },
+  }), tailwind()],
+  env: {
+    schema: {
+      STORYBLOK_TOKEN: envField.number({
+        context: "server",
+        access: "secret",
+        optional: false,
+      }),
+    },
+  },
+});
